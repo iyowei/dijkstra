913a9c2a1f4aa4954a7a9b961568900bb48dc4ac
itonyyo <swzyocowboy@icloud.com>
2022-01-11 19:33:51 +0800

    更新 try 脚本

--- try.js
+++ try.js
@@ -6,7 +6,6 @@ import {
   find,
   deleteNode,
   deleteDependence,
-  detect,
 } from './dijkstra.js';
 
 const graph = grapher();
@@ -15,29 +14,30 @@ graph.source = true;
 
 addEdge('one', 'two', 5, graph);
 addEdge('one', 'three', 2, graph);
-addEdge('three', 'two', 8, graph);
 
-addEdge('three', 'five', 7, graph);
+addEdge('three', 'two', 8, graph);
+addEdge('three', 'five', 1, graph); // 1/7
 
 addEdge('two', 'four', 4, graph);
-
 addEdge('two', 'five', 2, graph);
-addEdge('four', 'five', 6, graph);
 
+addEdge('four', 'five', 6, graph);
 addEdge('four', 'final', 3, graph);
+addEdge('four', 'what', 1, graph); // 多叉树
 
 addEdge('five', 'final', 1, graph);
+addEdge('five', 'final', 1, graph); // 重复添加
 
 // deleteNode('two', graph);
+// deleteNode('five', graph);
 
 const { path, weight } = find({
   startNode: 'one',
-  endNode: 'final',
+  endNode: 'what',
   graph,
 });
 
-// console.log(graph.pathCosts);
-// console.log(graph.processed);
-// console.log(graph.content);
+console.log(graph);
+console.log(graph.content);
 console.log('path', path);
-// console.log('weight', weight);
+console.log('weight', weight);
